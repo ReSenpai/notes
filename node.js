8aaac3423fa3d6f537688feb466472bd3c826993
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-05-08 03:30:54 +0300

    up

--- node.js
+++ node.js
@@ -2,11 +2,14 @@ function filteredArray(arr, elem) {
     let newArr = [];
     // Only change code below this line
     for (let i = 0; i < arr.length; i++) {
+      let check = 0;
       for (let g = 0; g < arr[i].length; g++) {
-
-        if (arr[i][g] !== elem) {
-            newArr.push(arr[i])
+        if (arr[i][g] == elem) {
+          check = 1;
         }
+      }
+      if (check == 0) {
+        newArr.push(arr[i])
       }
     }
     // Only change code above this line
