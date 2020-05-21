05113aa6894c5fd52afeebd79fcbb6e011dac730
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-05-21 03:30:35 +0300

    up

--- node.js
+++ node.js
@@ -1,7 +1,16 @@
-function chunkArrayInGroups(arr, size) {
-  return arr;
+function findOdd(A) {
+    
+  let newArr = [...A]
+
+  for (let i = 0; i < newArr.length; i++) {
+      const result = newArr.filter(num => num === newArr[i]);
+      console.log(result)
+  }
+  
+  newArr
+  
+  return 0;
 }
 
-
-console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]));
 
