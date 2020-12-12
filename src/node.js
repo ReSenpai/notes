fbf9251a59d15071a7be020a2eec4ed30baeff0d
Denis <bezbana1@gmail.com>
2020-12-12 14:49:43 +0300

    add Lupus task

--- src/node.js
+++ src/node.js
@@ -1,63 +1,30 @@
+function* sleep(ms, val) {
+    return yield new Promise(r => setTimeout(() => r(val), ms))
+}
+function* test(fl, time = 1e3) {
+    yield 0
+    return fl ? yield* sleep(time, 'reg') : yield 2
+}
+function* each() {
+    let sum = 0
+    for (let i = 0; i < 1e6; i++)
+        sum += yield* test()
+    return yield sum
+}
+function* test2() {
+    Promise.resolve().then(() => console.log('after'))
+    console.log('sum:', yield* each())
 
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-    const input = document.querySelector('.value').value;
-    console.log(input)
-})
+    Promise.resolve().then(() => console.log('before sleep'))
+    for (let i = 10; i--;)
+        console.log(`r${i}=`, yield* test(1, 100))
 
+    console.log('sum:', yield* each())
+}
 
-// var fullname = 'Mark Ivanov';
+asyncWrapper(test2)
 
-// var obj = {
-//     fullname: 'Joe Milner',
-//     prop: {
-//         fullname: 'David Smith',
-//         getFullname: function () {
-//             return this.fullname;
-//         }
-//     }
-// };
-
-// console.log(obj.prop.getFullname());
-
-// var test = obj.prop.getFullname;
-
-// console.log(test());
-
-/**
- * 1.  1
- * 2. 10
- * 3. undefined
- * 4. Double
- * 5. undefined
- * 6. Выведет - xyz Мы создаем экземпляр объекта и в качестве прототипа указывает объект Employee. Далее мы пытаемся удалить у объекта emp1 свойство company, но поскольку этого свойства нет в объекте emp1, то это не оказывает никакого эффекта. И в конце мы выводим в консоли свой company, объекта emp1. Движок сначала ищет это свой внутри объекта emp1, но не находит и спускается ниже по цепочке прототипов, пока свойство не будет найдено или мы не дойдем до Object.prototype, где поиск закончится неудачей. В данном слуае мы найдем это свой в первом же прототипе и выведем его значение в консоли.
- * 7. bar got called something
- * 8. for (var i = 1; i < 11; i++) {
-    function timer(num) {
-        setTimeout(function() {
-            console.log(num)
-        }, 1000 * num)
-    }
-    timer(i)
-}  
- * 9. var p = $('body > div:nth-child(2) > p:nth-child(3)');
- * 10. var pOnlyNum = $('p').filter(function() {
-            return this.textContent.match(/^\d+$/g);
-        })
- * 11. В данном случае подойдет вот такая функция: function getCardId(card) {     return card.match(/\d{4,16}$/g) }
- * 12. let newArray = array.map(num => num * 2);
- * 13. let newArray = array.map(user => user.name);
- * 14. let newArray = array.filter(num => num < 5);
- * 15. В логике функции withdrawAmountBy мы вызываем метод deductAmount объекта fooAccount, но при помощи метода call подставляем этому методу область видимости объекта barAccount.
-Вторым аргумент в методе call идет аргумент для withdrawAmountBy, то есть amount.
-Сам метод deductAmount хоть и создан в объекте fooAccount, в качестве уменьшаемого и возвращаемого значения использует this.amount, который берет контекст области видимости в зависимости от того где и как был вызван этот метод (присуще поведение динамической области видимости).
-Метод call во всей этой цепочке как раз и определяет для this текущую область видимости, из которой он возьмет amount.
-Задача метода deductAmount - вычитать из общего amount определенную сумму и затем возвращать текущее значение amount.
- 
-Именно поэтому при вызове метода withdrawAmountBy(400) значение уменьшается у объекта barAccount и уменьшается на 400. И во всех последующих вызовах уменьшается на задаваемое значение.
- * 16. 
- */
-
- // Код вернет [2, NaN, NaN, 2], потому что parseInt принимает 2 аргумента. 
-// 1 - это значение, которое необходимо проинтерпретировать, а второй - систему счисления
-// При этом метод map отдает parseInt в качестве первого аргумента текущий элемент, а в качестве второго - индекс этого элемента в массиве.
+function asyncWrapper(func) {
+    const it = func();
+    it.next();
+}
